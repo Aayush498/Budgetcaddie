300d4d06171aab9404e015730b12152fd2d14fe1
Aayush Jha <aayush59486jha@gmail.com>
2025-08-08 04:39:47 +0530

    Revert "fixed env issue"

--- src/main/java/com/budgetcaddie/BudgetcaddieApplication.java
+++ src/main/java/com/budgetcaddie/BudgetcaddieApplication.java
@@ -3,13 +3,10 @@ package com.budgetcaddie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication(scanBasePackages = "com.budgetcaddie")
 public class BudgetcaddieApplication {
 
 	public static void main(String[] args) {
-		Dotenv.load();
 		SpringApplication.run(BudgetcaddieApplication.class, args);
 	}
 
